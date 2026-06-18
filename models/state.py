320d05ebc04d9f7ec56f7e5767b3d2bd2b458de2
VaibhavMishra-21 <www.mevaibhav23032004@gmail.com>
2026-06-18 19:40:57 +0530

    Create state.py

---
+++ models/state.py
@@ -0,0 +1,17 @@
+from typing import TypedDict
+
+class SecurityState(TypedDict):
+
+    alert: str
+
+    classification: str
+
+    threat_analysis: str
+
+    malware_analysis: str
+
+    risk_level: str
+
+    response_plan: str
+
+    final_report: str
